9c609abb9299bf236664a783fdd6f60b6edb44e4
Aidan Cooper <aidcoope@iu.edu>
2023-04-20 11:57:33 -0400

    Implemented repositories

--- src/main/java/edu/iu/c322/mist/recommendations/repository/GameListRepository.java
+++ src/main/java/edu/iu/c322/mist/recommendations/repository/GameListRepository.java
@@ -1,4 +1,9 @@
 package edu.iu.c322.mist.recommendations.repository;
 
-public interface GameListRepository {
+import edu.iu.c322.mist.recommendations.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameListRepository extends JpaRepository<Game, Integer> {
 }
--- src/main/java/edu/iu/c322/mist/recommendations/repository/GenreListRepository.java
+++ src/main/java/edu/iu/c322/mist/recommendations/repository/GenreListRepository.java
@@ -1,4 +1,9 @@
 package edu.iu.c322.mist.recommendations.repository;
 
-public interface GenreListRepository {
+import edu.iu.c322.mist.recommendations.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreListRepository extends JpaRepository<Genre, Integer> {
 }
