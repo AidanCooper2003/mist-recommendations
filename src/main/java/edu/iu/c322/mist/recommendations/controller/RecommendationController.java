a586df661814cf299726b7cd8b56dcce181dda55
Aidan Cooper <aidcoope@iu.edu>
2023-04-20 11:58:45 -0400

    Added repositories for RecommendationController

--- src/main/java/edu/iu/c322/mist/recommendations/controller/RecommendationController.java
+++ src/main/java/edu/iu/c322/mist/recommendations/controller/RecommendationController.java
@@ -1,4 +1,13 @@
 package edu.iu.c322.mist.recommendations.controller;
 
+import edu.iu.c322.mist.recommendations.repository.GameListRepository;
+import edu.iu.c322.mist.recommendations.repository.GenreListRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/recommendations")
 public class RecommendationController {
+    private GameListRepository gameRepository;
+    private GenreListRepository genreRepository;
 }
